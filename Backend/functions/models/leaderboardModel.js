b0cb93f2c1c72ce32d0b7d58b9f71fd570e37917
Ulrich Mark <74579614+Alternality@users.noreply.github.com>
2024-06-13 13:15:13 +0800

    remove

--- Backend/functions/models/leaderboardModel.js
+++ Backend/functions/models/leaderboardModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const recipeSchema = require('../schema/recipe'); // Correct path to the recipe schema
+const leaderboardSchema = require('../schema/leaderboardSchema'); // Correct path to the recipe schema
 
 // Ensure that recipeSchema is a Mongoose schema instance
-if (!(recipeSchema instanceof mongoose.Schema)) {
-    throw new Error('recipeSchema is not a valid Mongoose schema');
+if (!(leaderboardSchema instanceof mongoose.Schema)) {
+    throw new Error('leaderboardSchema is not a valid Mongoose schema');
 }
 
-const RecipeModel = mongoose.model('Recipe', recipeSchema); // Create the Mongoose model
+const leaderboardModel = mongoose.model('Leaderboard', leaderboardSchema); // Create the Mongoose model
 
-module.exports = RecipeModel;
+module.exports = leaderboardModel;
